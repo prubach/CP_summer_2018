c9aca8ffdd5e9aba239ef1d3cddcc49a31891240
prubac <prubac@sgh.waw.pl>
2018-03-28 13:18:49 +0200

    28.03

---
+++ src/pl/waw/sgh/FrontTimes.java
@@ -0,0 +1,15 @@
+package pl.waw.sgh;
+
+public class FrontTimes {
+
+    public String frontTimes(String str, int n) {
+
+        return "abc";
+    }
+
+    public static void main(String[] args) {
+
+        String result = new FrontTimes().frontTimes("input", 2);
+
+    }
+}
---
+++ src/pl/waw/sgh/PrimeNumbers.java
@@ -0,0 +1,10 @@
+package pl.waw.sgh;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        int n = 10000;
+
+
+    }
+}
