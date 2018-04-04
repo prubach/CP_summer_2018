56707b49bf66226f19b2cb5946b67e24774071bf
prubac <prubac@sgh.waw.pl>
2018-04-04 10:22:56 +0200

    InputArgs

---
+++ src/pl/waw/sgh/InputArgs.java
@@ -0,0 +1,10 @@
+package pl.waw.sgh;
+
+public class InputArgs {
+
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println("Arg: " + arg);
+        }
+    }
+}
