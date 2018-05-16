f3dfe59c632f33e3c71e3c8065fe719087291deb
prubac <prubac@sgh.waw.pl>
2018-05-16 12:10:20 +0200

    Customer Form

---
+++ src/pl/waw/sgh/bank/ui/CustomerUI.java
@@ -0,0 +1,15 @@
+package pl.waw.sgh.bank.ui;
+
+import javax.swing.*;
+
+public class CustomerUI {
+    private JTextField idTextField;
+    private JTextField firstNameTextField;
+    private JTextField lastNameTextField;
+    private JTextField textField1;
+    private JButton prevButton;
+    private JButton deleteButton;
+    private JButton saveButton;
+    private JButton newButton;
+    private JButton nextButton;
+}
